6dc084d12f58a6e5684778667458e2d9331a2757
JGomez2023 <141684491+JGomez2023@users.noreply.github.com>
2023-09-22 11:25:32 -0300

    Add files via upload
    added files

---
+++ src/CompuertasLogicas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public interface IGate
+{
+    string GetName();
+    bool? Calculate();
+}
+public class Program
+{
+    static void Main()
+    {
+        // Ejemplo de uso:
+        GateAND andGate = new GateAND("AND1", 2);
+        andGate.SetInput(0, 1);
+        andGate.SetInput(1, 0);
+
+        GateNOT notGate = new GateNOT("NOT1");
+        notGate.SetInput(andGate);
+
+        bool? result = notGate.Calculate();
+        if (result.HasValue)
+        {
+            Console.WriteLine($"Resultado de {notGate.GetName()}: {result.Value}");
+        }
+        else
+        {
+            Console.WriteLine($"No se puede calcular el resultado de {notGate.GetName()}");
+        }
+
+        //Ejemplo de uso de GarageGate:
+        
+        GarageGate garageGate = new GarageGate();
+
+        // Prueba de las condiciones de apertura
+        Console.WriteLine("Puerta abierta: " + garageGate.OpenGate()); // Debe ser false
+
+        garageGate.PressButtonA();
+        garageGate.PressButtonB();
+        garageGate.PressButtonC();
+
+        Console.WriteLine("Puerta abierta: " + garageGate.OpenGate()); // Debe ser true
+
+        garageGate.ReleaseButtonA();
+        garageGate.ReleaseButtonB();
+        garageGate.ReleaseButtonC();
+
+        Console.WriteLine("Puerta abierta: " + garageGate.OpenGate()); // Debe ser false
+    }
+}
---
+++ src/Library/CompuertaAND.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public interface IGate
+{
+    string GetName();
+    bool? Calculate();
+}
+public class GateAND : IGate
+{
+    public string name;
+    public object[] inputs;
+
+    public GateAND(string nombre, int inputCount)
+    {
+        name = nombre;
+        inputs = new object[inputCount];
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetInput(int index, object value)
+    {
+        if (index >= 0 && index < inputs.Length)
+        {
+            inputs[index] = value;
+        }
+    }
+
+    public bool? Calculate()
+    {
+        int product = 1;
+        foreach (var input in inputs)
+        {
+            if (input is int intValue)
+            {
+                if (intValue == 0 || intValue == 1)
+                {
+                    product *= intValue;
+                }
+                else
+                {
+                    return null; // No se puede calcular si hay un valor no válido.
+                }
+            }
+            else if (input is IGate gate)
+            {
+                var result = gate.Calculate();
+                if (result.HasValue)
+                {
+                    product *= result.Value ? 1 : 0;
+                }
+                else
+                {
+                    return null; // No se puede calcular si una entrada es nula.
+                }
+            }
+        }
+        return product == 1;
+    }
+}
